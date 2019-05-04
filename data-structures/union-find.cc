b9a7826a5da9c5054951193ef0875810bd5d222d
손량 <loop.infinitely@gmail.com>
2019-05-04 11:21:23 +0900

    formatting

--- data-structures/union-find.cc
+++ data-structures/union-find.cc
@@ -19,7 +19,8 @@ public:
     int findSet(int i) { return (p[i] == i) ? i : (p[i] = findSet(p[i])); }
     bool isSameSet(int i, int j) { return findSet(i) == findSet(j); }
     void unionSet(int i, int j) {
-        if (!isSameSet(i, j)) { numSets--;
+        if (!isSameSet(i, j)) {
+            numSets--;
             int x = findSet(i), y = findSet(j);
             if (rank[x] > rank[y]) {
                 p[y] = x;
